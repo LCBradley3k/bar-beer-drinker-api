2460e4240125a57298c0e024f85afecef5818dbb
Mac User <BradleyMitchellis@gmail.com>
2018-11-17 22:55:22 -0500

    Fix spacing

--- BarBeerDrinker/database.py
+++ BarBeerDrinker/database.py
@@ -154,7 +154,7 @@ def find_top_drinkers(name):
 def find_drinker_bars_per_hour(name):
         with engine.connect() as con:
                 query = sql.text(
-                        "SELECT month, transaction_id, name, bar_name, MAX(total_per_month) as total_per_month FROM ( SELECT EXTRACT(MONTH FROM date) as month, orders.transaction_id, orders.name, orders.bar_name, SUM(total) as total_per_month FROM orders INNER JOIN transactions ON orders.transaction_id = transactions.transaction_id WHERE orders.name = :name GROUP BY month, orders.bar_name ORDER BY month, transactions.date,  transactions.time) A GROUP BY month;"
+                        "SELECT month, transaction_id, name, bar_name, MAX(total_per_month) as total_per_month FROM ( SELECT EXTRACT(MONTH FROM date) as month, orders.transaction_id, orders.name, orders.bar_name, SUM(total) as total_per_month FROM orders INNER JOIN transactions ON orders.transaction_id = transactions.transaction_id WHERE orders.name = :name GROUP BY month, orders.bar_name ORDER BY month, transactions.date, transactions.time) A GROUP BY month;"
                 )
                 rs = con.execute(query, name=name)
                 results = [dict(row) for row in rs]
