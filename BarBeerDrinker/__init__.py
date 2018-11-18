ff5adccd8d4e35e43f1741e0b68288cb69e28c1c
Mac User <BradleyMitchellis@gmail.com>
2018-11-17 21:53:03 -0500

    add distribution for drinkers per hour

--- BarBeerDrinker/__init__.py
+++ BarBeerDrinker/__init__.py
@@ -167,6 +167,20 @@ def find_top_drinkers(name):
     except Exception as e:
         return make_response(str(e), 500)
 
+@app.route("/api/top-drinker-bars-per-hour/<name>", methods=["GET"])
+def find_drinker_bars_per_hour(name):
+    try:
+        if name is None:
+            raise ValueError("Drinker is not specified.")
+        top_sales = database.find_drinker_bars_per_hour(name)
+        if top_sales is None:
+            return make_response("No drinker found with the given name.", 404)
+        return jsonify(top_sales)
+    except ValueError as e:
+        return make_response(str(e), 400)
+    except Exception as e:
+        return make_response(str(e), 500)
+
 
 #find beers cheaper than given number
 #used when getting input from user
